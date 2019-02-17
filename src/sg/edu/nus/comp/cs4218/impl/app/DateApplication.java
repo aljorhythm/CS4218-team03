5dbb6965509ce7caf3ec8ef578dae64324b9806d
Joakim Boman <boman96@live.se>
2019-02-17 22:17:44 +0800

    Add DateApplication tests

---
+++ src/sg/edu/nus/comp/cs4218/impl/app/DateApplication.java
@@ -0,0 +1,15 @@
+package sg.edu.nus.comp.cs4218.impl.app;
+
+import sg.edu.nus.comp.cs4218.app.DateInterface;
+
+/**
+ * Created by Joakim Boman on 2019-02-17.
+ */
+public class DateApplication implements DateInterface  {
+
+
+    @Override
+    public String getDate(String format) throws Exception {
+        return "";
+    }
+}
