de4e9c72355fef9eabe78d688bec59d59107368b
Joakim Boman <boman96@live.se>
2019-02-18 16:03:08 +0800

    Add first tests for WcApplication

---
+++ src/sg/edu/nus/comp/cs4218/impl/app/WcApplication.java
@@ -0,0 +1,28 @@
+package sg.edu.nus.comp.cs4218.impl.app;
+
+import sg.edu.nus.comp.cs4218.app.WcInterface;
+import sg.edu.nus.comp.cs4218.exception.AbstractApplicationException;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * Created by Joakim Boman on 2019-02-18.
+ */
+public class WcApplication implements WcInterface {
+
+    @Override
+    public String countFromFiles(Boolean isBytes, Boolean isLines, Boolean isWords, String... fileName) throws Exception {
+        return null;
+    }
+
+    @Override
+    public String countFromStdin(Boolean isBytes, Boolean isLines, Boolean isWords, InputStream stdin) throws Exception {
+        return null;
+    }
+
+    @Override
+    public void run(String[] args, InputStream stdin, OutputStream stdout) throws AbstractApplicationException {
+
+    }
+}
