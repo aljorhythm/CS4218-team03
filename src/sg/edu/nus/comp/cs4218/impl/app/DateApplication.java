5a12def98aa19e90d64b015ec00d86d128f58795
e0392448 <834767813@qq.com>
2019-02-19 22:37:07 +0800

    add the naive dateapp

--- src/sg/edu/nus/comp/cs4218/impl/app/DateApplication.java
+++ src/sg/edu/nus/comp/cs4218/impl/app/DateApplication.java
@@ -6,15 +6,33 @@ import sg.edu.nus.comp.cs4218.exception.EchoException;
 
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Calendar;
+import java.util.Date;
 
 public class DateApplication implements DateInterface {
     @Override
     public String getDate(String format) throws DateException {
+        Calendar calendar = Calendar.getInstance();
+        Date time = calendar.getTime();
+        if(format == null){
+            String str_time = time.toString();
+            return str_time;
+        }
         return null;
     }
 
+    /**
+     * Runs the cd application with the specified arguments.
+     * Assumption: The application must take in one arg. (cd without args is not supported)
+     *
+     * @param args   Array of arguments for the application.It may contain the format.
+     * @param stdin  An InputStream, not used.
+     * @param stdout An OutputStream, not used.
+     *
+     * @throws DateException
+     */
     @Override
     public void run(String[] args, InputStream stdin, OutputStream stdout) throws DateException {
-
+        System.out.println(getDate(null));
     }
 }
