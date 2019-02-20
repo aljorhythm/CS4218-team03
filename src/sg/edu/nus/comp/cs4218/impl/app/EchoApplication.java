ffaf07ed4c903dc8865e085c4338c8f2cbdeda9c
Joel Lim <103879u@gmail.com>
2019-02-20 14:47:57 +0800

    initial echo application tests

---
+++ src/sg/edu/nus/comp/cs4218/impl/app/EchoApplication.java
@@ -0,0 +1,19 @@
+package sg.edu.nus.comp.cs4218.impl.app;
+
+import sg.edu.nus.comp.cs4218.app.EchoInterface;
+import sg.edu.nus.comp.cs4218.exception.AbstractApplicationException;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class EchoApplication implements EchoInterface {
+    @Override
+    public String constructResult(String... args) throws Exception {
+        return null;
+    }
+
+    @Override
+    public void run(String[] args, InputStream stdin, OutputStream stdout) throws AbstractApplicationException {
+
+    }
+}
