769ef74a282038d4f18c1afd36cf7e55f9a493af
e0392448 <834767813@qq.com>
2019-02-25 03:45:45 +0800

    finish the implement of echo

--- src/sg/edu/nus/comp/cs4218/impl/app/EchoApplication.java
+++ src/sg/edu/nus/comp/cs4218/impl/app/EchoApplication.java
@@ -9,12 +9,24 @@ import java.io.OutputStream;
 
 public class EchoApplication implements EchoInterface {
     @Override
-    public String constructResult(String... args) throws Exception {
-        return null;
+    public String constructResult(String... args) throws EchoException {
+        StringBuilder res = new StringBuilder();
+        for (int i=1;i<args.length;i++){
+            if(i == args.length - 1){
+                res.append(args[i]);
+            }
+            else{
+                res.append(args[i]).append(" ");
+            }
+        }
+        return res.toString();
     }
 
     @Override
     public void run(String[] args, InputStream stdin, OutputStream stdout) throws EchoException {
-
+        if(!args[0].equals("echo") || args.length <= 1){
+            throw new EchoException("Invalid syntax.");
+        }
+        System.out.println(constructResult(args));
     }
 }
