82dc81bd23d8321a49c775e84f59065b675a12a3
Joel Lim <103879u@gmail.com>
2019-03-02 04:57:24 +0800

    init tests

--- src/sg/edu/nus/comp/cs4218/impl/app/LsApplication.java
+++ src/sg/edu/nus/comp/cs4218/impl/app/LsApplication.java
@@ -2,14 +2,40 @@ package sg.edu.nus.comp.cs4218.impl.app;
 
 import sg.edu.nus.comp.cs4218.app.LsInterface;
 import sg.edu.nus.comp.cs4218.exception.AbstractApplicationException;
+import sg.edu.nus.comp.cs4218.impl.util.StringUtils;
 
+import java.io.File;
+import java.io.FilenameFilter;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class LsApplication implements LsInterface {
+
+    static final FilenameFilter directoryFilter = new FilenameFilter() {
+        @Override
+        public boolean accept(File current, String name) {
+            // return new File(current, name).isDirectory();
+            return true;
+        }
+    };
+
     @Override
-    public String listFolderContent(Boolean isFoldersOnly, Boolean isRecursive, String... folderName) throws Exception {
-        return null;
+    public String listFolderContent(Boolean isFoldersOnly, Boolean isRecursive, String... folderNames) throws Exception {
+        Set<String> foldersContents = new LinkedHashSet<String>();
+        for(String folderName: folderNames) {
+            String[] folderContents = listFolderContent(isFoldersOnly, isRecursive, folderName);
+            foldersContents.addAll(Arrays.asList(folderContents));
+        }
+        return String.join(StringUtils.STRING_NEWLINE, foldersContents);
+    }
+
+    private String[] listFolderContent(Boolean isFoldersOnly, Boolean isRecursive, String folderName) throws Exception {
+        File file = new File(folderName);
+        String[] content = file.list(directoryFilter);
+        return content;
     }
 
     @Override
--- test/sg/edu/nus/comp/cs4218/impl/app/LsApplicationTest.java
+++ test/sg/edu/nus/comp/cs4218/impl/app/LsApplicationTest.java
@@ -1,22 +1,78 @@
 package sg.edu.nus.comp.cs4218.impl.app;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import sg.edu.nus.comp.cs4218.impl.util.StringUtils;
 
-class LsApplicationTest{
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+class LsApplicationTest {
     /**
-     * Configuration
+     * Configuration and data
      */
     LsApplication application;
+    static String test1_output = String.join(StringUtils.STRING_NEWLINE, new String[]{"folder_b/folder_c", "folder_c/folder_e", "folder_d"});
+    static String test2_output = String.join(StringUtils.STRING_NEWLINE, new String[]{"folder_c", "folder_d"});
+    static String test3_output = String.join(StringUtils.STRING_NEWLINE, new String[]{"folder_a", "folder_a/file_a", "folder_b/folder_c", "folder_b/folder_c/folder_e", "folder_b/folder_c/file_a", "folder_b/folder_d/", "folder_b/folder_d/file_b"});
+    static String test4_output = String.join(StringUtils.STRING_NEWLINE, new String[]{"folder_a", "folder_b"});
+    static String TEST_DIR = "./lsTestDir";
+    static String FOLDER_B = TEST_DIR + "/folder_b";
 
-    class TestCase {
-        boolean isFoldersOnly, isRecursive, folderName;
+    @BeforeEach
+    void init() {
+        application = new LsApplication();
     }
 
+    class TestCase {
+        boolean isFoldersOnly, isRecursive;
+        String folder, expected;
+
+        TestCase isFoldersOnly(boolean arg) {
+            isFoldersOnly = arg;
+            return this;
+        }
+
+        TestCase isRecursive(boolean arg) {
+            this.isRecursive = arg;
+            return this;
+        }
+
+        TestCase folderName(String folder) {
+            this.folder = folder;
+            return this;
+        }
+
+        TestCase expected(String expected) {
+            this.expected = expected;
+            return this;
+        }
+
+        void run() throws Exception {
+            String actual = application.listFolderContent(isFoldersOnly, isRecursive, folder);
+            assertEquals(actual, expected);
+        }
+    }
+
+    TestCase newCase() {
+        return new TestCase();
+    }
+
+    /**
+     * Tests
+     */
     @Test
-    void listFolderContent() {
+    void listFolderContent_test1() throws Exception {
+        newCase()
+                .isFoldersOnly(true)
+                .isRecursive(false)
+                .folderName(FOLDER_B)
+                .expected(test1_output)
+                .run();
     }
 
     @Test
     void run() {
+        fail("todo");
     }
 }
