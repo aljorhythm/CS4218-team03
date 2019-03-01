482177efac026385190ed89d582720c02b089928
Joel Lim <103879u@gmail.com>
2019-03-01 17:44:28 +0800

    script

---
+++ scripts/code-generator/join_java_strings.py
@@ -0,0 +1,14 @@
+# converts lines of strings into java code snippet:
+# String.join(StringUtils.NEW_LINE, new String[]{ .... });
+
+import sys
+filename = sys.argv[1]
+with open(filename) as file:
+    lines = file.readlines()
+    sanitized = [s.replace("\n", "") for s in lines]
+    var_name = sanitized[0]
+    sanitized = sanitized[1:]
+    wrapped = [ '"{}"'.format(s) for s in sanitized]
+    joined = ",".join(wrapped)
+    output = 'static String {} = String.join(StringUtils.NEW_LINE, new String[]{{{}}});'.format(var_name, joined)
+    print(output)
