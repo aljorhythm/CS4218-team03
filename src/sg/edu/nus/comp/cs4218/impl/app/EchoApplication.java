65aa46b8abce484eba28109d75fc01fbbafaa041
Joel Lim <103879u@gmail.com>
2019-03-02 18:33:24 +0800

    init tests

--- src/sg/edu/nus/comp/cs4218/impl/app/EchoApplication.java
+++ src/sg/edu/nus/comp/cs4218/impl/app/EchoApplication.java
@@ -3,10 +3,16 @@ package sg.edu.nus.comp.cs4218.impl.app;
 import sg.edu.nus.comp.cs4218.app.EchoInterface;
 import sg.edu.nus.comp.cs4218.exception.AbstractApplicationException;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+import static sg.edu.nus.comp.cs4218.impl.util.StringUtils.CHARSET_UTF8;
+
 public class EchoApplication implements EchoInterface {
+    public static final String FAIL_ECHO = "fail_echo";
+    public static final String FAIL_ECHO_WRITE = "fail_echo_write";
+
     @Override
     public String constructResult(String... args) throws Exception {
         return null;
@@ -14,6 +20,17 @@ public class EchoApplication implements EchoInterface {
 
     @Override
     public void run(String[] args, InputStream stdin, OutputStream stdout) throws AbstractApplicationException {
+        String result;
+        try {
+            result = this.constructResult(args);
+        } catch (Exception e) {
+            throw new AbstractApplicationException(FAIL_ECHO){};
+        }
 
+        try {
+            stdout.write(result.getBytes(CHARSET_UTF8));
+        } catch (IOException e) {
+            throw new AbstractApplicationException(FAIL_ECHO_WRITE){};
+        }
     }
 }
--- test/sg/edu/nus/comp/cs4218/impl/app/EchoApplicationTest.java
+++ test/sg/edu/nus/comp/cs4218/impl/app/EchoApplicationTest.java
@@ -3,36 +3,41 @@ package sg.edu.nus.comp.cs4218.impl.app;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.ByteArrayOutputStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+import static sg.edu.nus.comp.cs4218.impl.util.StringUtils.CHARSET_UTF8;
 
 /**
- echo
- Description
- The echo command writes its arguments separated by spaces and terminates by a newline on the standard output.
- Command format
- echo [ARG]
- ARG – list of arguments
- Examples
- # Display A B C (separated by space characters)
- $ echo A B C
- # Display “A*B*C” (separated by *)
- $ echo “A*B*C”
+ * echo
+ * Description
+ * The echo command writes its arguments separated by spaces and terminates by a newline on the standard output.
+ * Command format
+ * echo [ARG]
+ * ARG – list of arguments
+ * Examples
+ * # Display A B C (separated by space characters)
+ * $ echo A B C
+ * # Display “A*B*C” (separated by *)
+ * $ echo “A*B*C”
  **/
 class EchoApplicationTest {
 
     EchoApplication application;
 
     @BeforeEach
-    public void initApplication(){
+    public void initApplication() {
         application = new EchoApplication();
     }
 
     /**
-     * TODO description
-     * @param expect
-     * @param params
+     * Asserts echo with input parameters produces expected output
+     *
+     * @param expect expected result
+     * @param params input strings
      */
-    private void assertParamsAndExpected(String expect, String... params){
+    private void assertParamsAndExpected(String expect, String... params) {
         String actual = null;
         try {
             actual = application.constructResult(params);
@@ -43,7 +48,7 @@ class EchoApplicationTest {
     }
 
     /**
-     * TODO description
+     * Echo nothing
      */
     @Test
     public void echoNothing() {
@@ -52,11 +57,42 @@ class EchoApplicationTest {
     }
 
     /**
-     * TODO description
+     * Echo strings
      */
     @Test
     public void echoStrings() {
         assertParamsAndExpected("abc", "abc");
-        assertParamsAndExpected("abc cde.", new String[]{ "abc", "cde." });
+        assertParamsAndExpected("abc cde.", new String[]{"abc", "cde."});
+    }
+
+    /**
+     * Tests with no input and output streams
+     *
+     * @throws Exception
+     */
+    public void run_noInputOutput() throws Exception {
+        application.run(new String[]{}, null, null);
+    }
+
+    /**
+     * Tests with no args and input and output streams
+     *
+     * @throws Exception
+     */
+    public void run_noArgs_noInputOutput() throws Exception {
+        application.run(null, null, null);
+    }
+
+    /**
+     * Test running of echo
+     * @throws Exception
+     */
+    public void run() throws Exception {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        String[] stringList = new String[]{"abc", "cde"};
+        application.run(stringList, null, outputStream);
+        String actual = outputStream.toString(CHARSET_UTF8);
+        String expected = String.join(CHARSET_UTF8, stringList);
+        assertEquals(actual, expected);
     }
 }
