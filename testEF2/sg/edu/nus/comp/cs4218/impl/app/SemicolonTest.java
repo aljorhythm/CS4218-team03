8a412e3e4aae359d1afd63e32a41e5491469206f
e0392448 <834767813@qq.com>
2019-03-03 00:46:31 +0800

    Add the basic testcases for semicolon.

--- testEF2/sg/edu/nus/comp/cs4218/impl/app/SemicolonTest.java
+++ testEF2/sg/edu/nus/comp/cs4218/impl/app/SemicolonTest.java
@@ -1,9 +1,13 @@
 package sg.edu.nus.comp.cs4218.impl.app;
 
 import org.junit.jupiter.api.Test;
+import sg.edu.nus.comp.cs4218.Command;
+import sg.edu.nus.comp.cs4218.exception.AbstractApplicationException;
 import sg.edu.nus.comp.cs4218.exception.ShellException;
 import sg.edu.nus.comp.cs4218.impl.cmd.CallCommand;
 import sg.edu.nus.comp.cs4218.impl.cmd.SequenceCommand;
+import sg.edu.nus.comp.cs4218.impl.util.ApplicationRunner;
+import sg.edu.nus.comp.cs4218.impl.util.CommandBuilder;
 
 import java.io.*;
 import java.util.Arrays;
@@ -14,6 +18,10 @@ import static sg.edu.nus.comp.cs4218.exception.ShellException.INVALID_INPUT_STRE
 import static sg.edu.nus.comp.cs4218.impl.util.StringUtils.CHARSET_UTF8;
 
 class SemicolonTest {
+    Command command;
+    ByteArrayOutputStream baoStream = new ByteArrayOutputStream();
+    String echoSemicolonStringWithSpace = "echo a ; echo b";
+    String echoSemicolonStringWithoutSpace = "echo a;echo b";
     /**
      * Mocks a call command
      * Simply writes input from stdin and "ABC" to output
@@ -95,7 +103,7 @@ class SemicolonTest {
 
         SequenceCommand sequenceCommand = new SequenceCommand(Arrays.asList(commands));
         sequenceCommand.evaluate(inputStream, outputStream);
-        String expected = "test data ABCDEF";
+        String expected = "test data ABC\nDEF\n";
         String actual = ((ByteArrayOutputStream) outputStream).toString("UTF-8");
         assertEquals(actual, expected);
     }
@@ -115,8 +123,27 @@ class SemicolonTest {
 
         SequenceCommand sequenceCommand = new SequenceCommand(Arrays.asList(commands));
         sequenceCommand.evaluate(inputStream, outputStream);
-        String expected = "test data DEFABC";
+        String expected = "test data DEF\nABC\n";
         String actual = ((ByteArrayOutputStream) outputStream).toString("UTF-8");
         assertEquals(actual, expected);
+    }
+
+    /**
+     * Test if the shell can get the semicolon
+     */
+    @Test
+    void semicolonWithoutSpace() throws AbstractApplicationException, ShellException, IOException {
+        command = CommandBuilder.parseCommand(echoSemicolonStringWithoutSpace,new ApplicationRunner());
+        command.evaluate(System.in,baoStream);
+        assertEquals("a b",baoStream.toString());
+        baoStream.flush();
+    }
+
+    @Test
+    void semicolonWithSpace() throws AbstractApplicationException, ShellException, IOException {
+        command = CommandBuilder.parseCommand(echoSemicolonStringWithSpace,new ApplicationRunner());
+        command.evaluate(System.in,baoStream);
+        assertEquals("a b",baoStream.toString());
+        baoStream.flush();
     }
 }
