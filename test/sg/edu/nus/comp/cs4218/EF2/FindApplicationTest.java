9b71019437a81d807393af135d143a585fdb27ca
Joakim Boman <boman96@live.se>
2019-03-02 21:26:44 +0800

    Move EF2 tests

--- test/sg/edu/nus/comp/cs4218/EF2/FindApplicationTest.java
+++
@@ -1,9 +0,0 @@
-package sg.edu.nus.comp.cs4218.EF2;
-
-import sg.edu.nus.comp.cs4218.impl.app.FindApplication;
-
-public class FindApplicationTest {
-    FindApplication findInterface = new FindApplication();
-
-
-}
---
+++ testEF2/sg/edu/nus/comp/cs4218/impl/app/FindApplicationTest.java
@@ -0,0 +1,7 @@
+package sg.edu.nus.comp.cs4218.impl.app;
+
+public class FindApplicationTest {
+    FindApplication findApplication = new FindApplication();
+
+
+}
