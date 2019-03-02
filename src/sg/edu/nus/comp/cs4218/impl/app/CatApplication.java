644154db29ea354a800027040496a109ad0159e3
Joel Lim <103879u@gmail.com>
2019-03-03 01:55:02 +0800

    pass testCatStdinEmptyInputSuccess

--- src/sg/edu/nus/comp/cs4218/impl/app/CatApplication.java
+++ src/sg/edu/nus/comp/cs4218/impl/app/CatApplication.java
@@ -2,13 +2,16 @@ package sg.edu.nus.comp.cs4218.impl.app;
 
 import sg.edu.nus.comp.cs4218.app.CatInterface;
 import sg.edu.nus.comp.cs4218.exception.CatException;
+import sg.edu.nus.comp.cs4218.impl.util.IOUtils;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
 public class CatApplication implements CatInterface {
     public static final String ERR_IS_DIR = "This is a directory";
     public static final String ERR_READING_FILE = "Could not read file";
+    public static final String ERR_READING_STREAM = "Could not read stream";
     public static final String ERR_WRITE_STREAM = "Could not write to output stream";
     public static final String ERR_NULL_STREAMS = "Null Pointer Exception";
     public static final String ERR_GENERAL = "Exception Caught";
@@ -21,7 +24,6 @@ public class CatApplication implements CatInterface {
      * @param stdin  An InputStream. The input for the command is read from this InputStream if no
      *               files are specified.
      * @param stdout An OutputStream. The output of the command is written to this OutputStream.
-     *
      * @throws CatException If the file(s) specified do not exist or are unreadable.
      */
     @Override
@@ -36,9 +38,17 @@ public class CatApplication implements CatInterface {
 
     @Override
     public String catStdin(InputStream stdin) throws CatException {
-        if(stdin == null) {
-           throw new CatException(ERR_NULL_STREAMS);
+        if (stdin == null) {
+            throw new CatException(ERR_NULL_STREAMS);
         }
-        return null;
+
+        String inputString;
+        try {
+            inputString = IOUtils.stringFromInputStream(stdin);
+        } catch (IOException e) {
+            throw new CatException(ERR_READING_STREAM);
+        }
+
+        return inputString;
     }
 }
