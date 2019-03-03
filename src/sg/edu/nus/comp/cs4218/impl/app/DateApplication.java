05635458405d49cec279d9d5716cce0e28008631
xjh <e0392487@u.nus.edu>
2019-03-03 22:49:24 +0800

    io-impl

--- src/sg/edu/nus/comp/cs4218/impl/app/DateApplication.java
+++ src/sg/edu/nus/comp/cs4218/impl/app/DateApplication.java
@@ -1,6 +1,10 @@
 package sg.edu.nus.comp.cs4218.impl.app;
 
 import sg.edu.nus.comp.cs4218.app.DateInterface;
+import sg.edu.nus.comp.cs4218.exception.AbstractApplicationException;
+
+import java.io.InputStream;
+import java.io.OutputStream;
 
 /**
  * Created by Joakim Boman on 2019-02-17.
@@ -11,5 +15,10 @@ public class DateApplication implements DateInterface  {
     @Override
     public String getDate(String format) throws Exception {
         return "";
+    }
+
+    @Override
+    public void run(String[] args, InputStream stdin, OutputStream stdout) throws AbstractApplicationException {
+
     }
 }
