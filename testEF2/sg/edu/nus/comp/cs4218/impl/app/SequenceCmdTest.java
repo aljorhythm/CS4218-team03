729da8a48f63a7093857169f4fd81dae219ebb2d
e0392448 <834767813@qq.com>
2019-03-04 12:56:39 +0800

    Fix globbing test and substitution test

--- testEF2/sg/edu/nus/comp/cs4218/impl/app/SequenceCmdTest.java
+++ testEF2/sg/edu/nus/comp/cs4218/impl/app/SequenceCmdTest.java
@@ -1,13 +1,9 @@
 package sg.edu.nus.comp.cs4218.impl.app;
 
 import org.junit.jupiter.api.Test;
-import sg.edu.nus.comp.cs4218.Command;
-import sg.edu.nus.comp.cs4218.exception.AbstractApplicationException;
 import sg.edu.nus.comp.cs4218.exception.ShellException;
 import sg.edu.nus.comp.cs4218.impl.cmd.CallCommand;
 import sg.edu.nus.comp.cs4218.impl.cmd.SequenceCommand;
-import sg.edu.nus.comp.cs4218.impl.util.ApplicationRunner;
-import sg.edu.nus.comp.cs4218.impl.util.CommandBuilder;
 
 import java.io.*;
 import java.util.Arrays;
@@ -17,11 +13,7 @@ import static org.mockito.Mockito.mock;
 import static sg.edu.nus.comp.cs4218.exception.ShellException.INVALID_INPUT_STREAM;
 import static sg.edu.nus.comp.cs4218.impl.util.StringUtils.CHARSET_UTF8;
 
-class SemicolonTest {
-    Command command;
-    ByteArrayOutputStream baoStream = new ByteArrayOutputStream();
-    String echoSemicolonStringWithSpace = "echo a ; echo b";
-    String echoSemicolonStringWithoutSpace = "echo a;echo b";
+class SequenceCmdTest {
 
     /**
      * Mocks a call command
@@ -97,24 +89,5 @@ class SemicolonTest {
         String expected = "test data DEF\nABC\n";
         String actual = ((ByteArrayOutputStream) outputStream).toString("UTF-8");
         assertEquals(actual, expected);
-    }
-
-    /**
-     * Test if the shell can get the semicolon
-     */
-    @Test
-    void semicolonWithoutSpace() throws AbstractApplicationException, ShellException, IOException {
-        command = CommandBuilder.parseCommand(echoSemicolonStringWithoutSpace,new ApplicationRunner());
-        command.evaluate(System.in,baoStream);
-        assertEquals("a b",baoStream.toString());
-        baoStream.flush();
-    }
-
-    @Test
-    void semicolonWithSpace() throws AbstractApplicationException, ShellException, IOException {
-        command = CommandBuilder.parseCommand(echoSemicolonStringWithSpace,new ApplicationRunner());
-        command.evaluate(System.in,baoStream);
-        assertEquals("a b",baoStream.toString());
-        baoStream.flush();
     }
 }
