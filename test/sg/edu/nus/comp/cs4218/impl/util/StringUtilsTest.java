d7dbba42de772ec8fdb75f0d31df069314532c20
Joel Lim <103879u@gmail.com>
2019-03-21 15:33:45 +0800

    add comments

--- test/sg/edu/nus/comp/cs4218/impl/util/StringUtilsTest.java
+++ test/sg/edu/nus/comp/cs4218/impl/util/StringUtilsTest.java
@@ -6,18 +6,27 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StringUtilsTest {
 
+    /**
+     * String is blank
+     */
     @Test
     void isBlank() {
         String str = "";
         assertTrue(StringUtils.isBlank(str));
     }
 
+    /**
+     * String is not blank
+     */
     @Test
     void isBlankFalse() {
         String str = "asd";
         assertFalse(StringUtils.isBlank(str));
     }
 
+    /**
+     * Character should be multiplied
+     */
     @Test
     void multiplyChar() {
         char chr = 'c';
@@ -25,6 +34,9 @@ class StringUtilsTest {
         assertEquals(expected, StringUtils.multiplyChar(chr, 5));
     }
 
+    /**
+     * String should be tokenized
+     */
     @Test
     void tokenize() {
         String[] tokens = StringUtils.tokenize("asd 123 \\* '*'");
@@ -32,12 +44,27 @@ class StringUtilsTest {
         assertArrayEquals(expected, tokens);
     }
 
+    /**
+     * String is representation of integer
+     */
     @Test
     void isNumbericTrue() {
         String str = "123";
         assertTrue(StringUtils.isNumberic(str));
     }
 
+    /**
+     * String is representation of integer
+     */
+    @Test
+    void isNumbericTrueNegative() {
+        String str = "-123";
+        assertTrue(StringUtils.isNumberic(str));
+    }
+
+    /**
+     * String is not a representation of integer
+     */
     @Test
     void isNumbericFalse() {
         String str = "abc";
@@ -46,18 +73,27 @@ class StringUtilsTest {
         assertFalse(StringUtils.isNumberic(str));
     }
 
+    /**
+     * First word in string is a representation of integer
+     */
     @Test
     void getFirstNum() {
         String str = "123 asdas";
         assertEquals(123, StringUtils.getFirstNum(str));
     }
 
+    /**
+     * First word in string is a representation of negative integer
+     */
     @Test
     void getFirstNumNegative() {
         String str = "-123 asdas";
         assertEquals(-123, StringUtils.getFirstNum(str));
     }
 
+    /**
+     * First word in string is not representation of integer
+     */
     @Test
     void getFirstNumError() {
         String str = "asdas";
@@ -66,6 +102,9 @@ class StringUtilsTest {
         });
     }
 
+    /**
+     * Strings should be prepended with "13"
+     */
     @Test
     void prependStringToStrings() {
         String[] strs = {"abc", "cde"};
