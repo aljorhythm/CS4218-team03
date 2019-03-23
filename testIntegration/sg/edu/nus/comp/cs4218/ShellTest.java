36472e8f3c8444ac5bdd75f61c4e7041fa783ea1
Joel Lim <103879u@gmail.com>
2019-03-23 16:23:03 +0800

    initialize shell integration tests

---
+++ testIntegration/sg/edu/nus/comp/cs4218/ShellTest.java
@@ -0,0 +1,27 @@
+package sg.edu.nus.comp.cs4218;
+
+import sg.edu.nus.comp.cs4218.exception.ShellException;
+import sg.edu.nus.comp.cs4218.impl.ShellImpl;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static sg.edu.nus.comp.cs4218.impl.util.IOUtils.stringsToInputStream;
+import static sg.edu.nus.comp.cs4218.impl.util.StringUtils.STRING_NEWLINE;
+
+public class ShellTest {
+    /**
+     * Tests running of shell with input and output
+     */
+    void assertInputOutput(String[] commands, String[] expectedLines) throws IOException, ShellException {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        InputStream inputStream = stringsToInputStream(commands);
+        Shell shell = new ShellImpl(inputStream, outputStream);
+        shell.run();
+        String actual = outputStream.toString();
+        String expected = String.join(STRING_NEWLINE, expectedLines);
+        assertEquals(expected, actual);
+    }
+}
