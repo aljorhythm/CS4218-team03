fbf04917555276a24e599a22856891f22c57c2c3
e0392448 <834767813@qq.com>
2019-03-29 10:45:53 +0800

    update exit test

--- test/sg/edu/nus/comp/cs4218m1/impl/app/ExitApplicationTest.java
+++ test/sg/edu/nus/comp/cs4218m1/impl/app/ExitApplicationTest.java
@@ -1,25 +1,76 @@
 package sg.edu.nus.comp.cs4218m1.impl.app;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import sg.edu.nus.comp.cs4218.exception.ExitException;
 import sg.edu.nus.comp.cs4218.impl.app.ExitApplication;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.security.Permission;
+
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.spy;
 
 class ExitApplicationTest {
 
     ExitApplication exitApplication;
+    private static InputStream mockIs;
+    private static OutputStream mockOs;
+
+    /**
+     * Mock SecurityManager that prevents SecurityManager from calling System.exit()
+     * Throws SecurityException when checkExit(int status) is called
+     */
+    private static class NoExitSecurityManager extends SecurityManager {
+        @Override
+        public void checkPermission(Permission perm) {
+            //No restrictions
+        }
+
+        @Override
+        public void checkPermission(Permission perm, Object context) {
+            //No restrictions
+        }
+
+        @Override
+        public void checkExit(int status) {
+            super.checkExit(status);
+            throw new SecurityException(Integer.toString(status));
+        }
+    }
 
     @BeforeEach
     void setUp() {
-        exitApplication = new ExitApplication();
+        mockIs = Mockito.mock(InputStream.class);
+        mockOs = Mockito.mock(OutputStream.class);
+        exitApplication = spy(new ExitApplication());
+        System.setSecurityManager(new NoExitSecurityManager());
+    }
+
+    @AfterAll
+    static void tearDown() {
+        System.setSecurityManager(null);
     }
 
     @Test
     void testExitDefaultInputSuccess() {
-        assertThrows(ExitException.class, () -> {
-            exitApplication.run(new String[0], System.in, System.out);
+        Exception actualException = assertThrows(ExitException.class, () -> {
+            exitApplication.run(new String[0], mockIs, mockOs);
         });
+        // please add the related exception messages here.
+//        assertEquals(EXIT_SUCCESS, actualException.getMessage());
+    }
+
+
+    @Test
+    void testRunExitWithMultipleArguments() {
+        String[] args = new String[]{"exit", "exit2", "exit3"};
+        Exception actualException = assertThrows(ExitException.class, () ->
+                exitApplication.run(args, mockIs, mockOs));
+        // please add the related exception messages here.
+//        assertEquals(MULTIPLE_ARGS, actualException.getMessage());
     }
 }
