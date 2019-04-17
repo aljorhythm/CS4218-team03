b9bfdf82bee269150f22d754c86d6da54e652cb7
Joakim Boman <boman96@live.se>
2019-04-17 20:36:59 +0800

    Fix from8_39 bug

---
+++ Hackathon/sg/edu/nus/comp/cs4218/bugfixes/GrepTest.java
@@ -0,0 +1,34 @@
+package sg.edu.nus.comp.cs4218.bugfixes;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import sg.edu.nus.comp.cs4218.exception.GrepException;
+import sg.edu.nus.comp.cs4218.impl.app.GrepApplication;
+import sg.edu.nus.comp.cs4218.impl.util.IOUtils;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GrepTest {
+    GrepApplication grepApp;
+
+    /**
+     * Initializes application for each test
+     */
+    @BeforeEach
+    public void initApplication() {
+        grepApp = new GrepApplication();
+    }
+
+    @Test
+    public void runInvalidFlagDash() throws IOException {
+        ByteArrayOutputStream stdout = new ByteArrayOutputStream();
+        InputStream stdin = IOUtils.stringToInputStream("");
+        String[] args = {"-c", "-i", "-", ".*LINE.*", "file1"};
+        assertThrows(GrepException.class, () -> grepApp.run(args, stdin, stdout));
+    }
+
+}
